9616be16c5dfa9fed125fb86df883a31a3eb68e4
Daniel Tull <dt@danieltull.co.uk>
2020-05-06 18:26:12 +0100

    Move the string initializer to the main model definition

--- Sources/MarkdownKit/Markdown.swift
+++ Sources/MarkdownKit/Markdown.swift
@@ -6,6 +6,10 @@ public struct Markdown {
     public init(content: [Block]) {
         self.content = content
     }
+
+    public init(string: String) throws {
+        try self.init(Node(markdown: string))
+    }
 }
 
 // MARK: - Equatable
--- Sources/MarkdownKit/Parsing/Markdown+Node.swift
+++ Sources/MarkdownKit/Parsing/Markdown+Node.swift
@@ -3,10 +3,6 @@ import cmark
 
 extension Markdown {
 
-    public init(string: String) throws {
-        try self.init(Node(markdown: string))
-    }
-
     init(_ node: Node) throws {
         struct NotDocumentType: Error {}
         guard node.type == CMARK_NODE_DOCUMENT else { throw NotDocumentType() }
